91282f2bf07939ff37d72b37404a3aff84271eac
th309 <83997270+th309@users.noreply.github.com>
2026-03-24 07:22:34 -0400

    refactor: add barrel exports for new map utilities

--- packages/frontend/app/map/utils/index.ts
+++ packages/frontend/app/map/utils/index.ts
@@ -1,6 +1,11 @@
 // Map utilities barrel exports
-export * from './colorScale';
-export * from './metricUtils';
+export * from "./colorScale";
+export * from "./metricUtils";
+export * from "./polylabel";
+export * from "./label-layout";
+export * from "./callout-markers";
+export * from "./value-format-expressions";
+export * from "./map-layer-config";
 
 // Re-export key config items for convenience
-export { fetchMetricData, toHomeValues } from '../config';
+export { fetchMetricData, toHomeValues } from "../config";
