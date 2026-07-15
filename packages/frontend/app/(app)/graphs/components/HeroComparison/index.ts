4fdf8a50227215191f4834566f95b76e40f3f988
th309 <83997270+th309@users.noreply.github.com>
2026-07-15 07:29:48 -0400

    chore(graphs): delete abandoned V1 graphs dead-code subgraph

--- packages/frontend/app/(app)/graphs/components/HeroComparison/index.ts
+++
@@ -1,5 +0,0 @@
-export { HeroComparison } from './HeroComparison';
-export { ScoreShowdown } from './ScoreShowdown';
-export { PriorityBreakdown } from './PriorityBreakdown';
-export { TemplateTabs } from './TemplateTabs';
-export { TemplateVisualization } from './TemplateVisualization';
